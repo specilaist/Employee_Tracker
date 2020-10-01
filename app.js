60580bde7a959652e1f0ee13433166a7ba1c6159
specilaist <specilaist@msn.com>
2020-10-01 15:59:27 -0700

    created prompt to make new employee in app.js

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
 const Employee = require('./employeeClass/Employee');
-const Manager = require('./employeeClass/Manager');
+// const Manager = require('./employeeClass/Manager');
 const inquirer = require('inquirer');
 const mySql = require('mysql');
 
+const employeeLog = [];
 
+const startApp = () => {
+      inquirer.prompt([
+            {
+                  name: 'start',
+                  type: 'confirm',
+                  message: 'Would you like to create a new Employee?',
+            }
+      ]).then((answer) => {
+            newEmployee();
+      });
+};
 
+function newEmployee() {
+      inquirer.prompt([
+            {
+                  name: 'firstName',
+                  type: 'input',
+                  message: 'Please input your First Name'
+            },
+            {
+                  name: 'lastName',
+                  type: 'input',
+                  message: 'Please input your Last Name'
+
+            },
+            {
+                  name: 'role',
+                  type: 'input',
+                  message: 'Please tell us your role'
+            },
+            {
+                  name: 'managerId',
+                  type: 'input',
+                  message: 'What is your manager Id?'
+            }
+
+      ]).then((answer) => {
+            const employee = new Employee(answer.firstName, answer.lastName, answer.role, answer. managerId);
+            console.log(employee);
+            employeeLog.push(employee);
+      })
+};
+
+startApp();
